d3e48250e08c19499baae34d20f9d8cf0b16a325
Ing. Davidson ADRIEN <ing.adriendavidson07@gmail.com>
2025-08-07 14:48:31 -0400

    El Juego Amigo Secreto en JS.

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let amigo = document.getElementById("amigo");
+let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+
+function agregarAmigo(){
+    amigoAgregado = amigo.value;
+    if (amigoAgregado == "") {
+        alert("Por favor, inserte un nombre valido.");
+    } else {
+        amigos.push(amigoAgregado);
+        amigo.value = "";
+        updateFriendList();
+    }
+}
+
+function updateFriendList(){
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i  < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+        
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert("Primero agrega amigos para sortear");
+    } else {
+        resultado.innerHTML = "";
+        let nombresAleatorios = Math.floor(Math.random() * amigos.length);
+        let li = document.createElement("li");
+        li.textContent = `El amigo secreto es ${amigos[nombresAleatorios]}`
+        resultado.appendChild(li);
+    }
+}
